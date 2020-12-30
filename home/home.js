a3a6fe5fd5cada1c0950fe17a4404484afc77849
Scarecrow94 <adesuyiayo@rocketmail.com>
2020-12-30 09:36:44 +0100

    finished all layouts, waiting to port to vue

--- home/home.js
+++ home/home.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
   //navbar scroll animation
   $(window).scroll(function () {
+    //show nav background
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
+
+    //scroll to top button visibility
+    if (this.scrollY > 550) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  //slide up when arrow is clicked
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
   });
 
   //menu hide and unhide
@@ -34,5 +47,43 @@ $(document).ready(function () {
         nav: false,
       },
     },
+  });
+
+  //quick scroll animation
+  $(".scroll-up-btn").hover(
+    function () {
+      $(".scroll-up-btn i").addClass("bounce-4");
+    },
+    function () {
+      $(".scroll-up-btn i").removeClass("bounce-4");
+    }
+  );
+
+  //typed animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "Software Engineer",
+      "Android Developer",
+      "Game Developer",
+      "Professional Gamer",
+      "Project Manager",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  //typed animation script 2nd
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Software Engineer",
+      "Android Developer",
+      "Game Developer",
+      "Professional Gamer",
+      "Project Manager",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
   });
 });
